8465ed9c76a8bc73b2541077809a16fd7b75bb7d
bthnkucuk <84974284+bthnkucuk@users.noreply.github.com>
2022-11-07 20:43:44 +0300

    fix

--- lib/pages/main_pages/settings_page/view/settings_view.dart
+++ lib/pages/main_pages/settings_page/view/settings_view.dart
@@ -1,4 +1,3 @@
-import 'package:base_application/main.dart';
 import 'package:base_application/pages/main_pages/settings_page/controller/settings_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,9 +11,15 @@ class SettingsView extends GetView<SettingsController> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          IconButton(onPressed: () => controller.changeTheme(ThemeMode.dark), icon: const Icon(Icons.dark_mode)),
-          IconButton(onPressed: () => controller.changeTheme(ThemeMode.light), icon: const Icon(Icons.light_mode)),
-          IconButton(onPressed: () => controller.changeTheme(ThemeMode.system), icon: const Icon(Icons.settings)),
+          IconButton(
+              onPressed: () => controller.changeTheme(ThemeMode.dark),
+              icon: const Icon(Icons.dark_mode)),
+          IconButton(
+              onPressed: () => controller.changeTheme(ThemeMode.light),
+              icon: const Icon(Icons.light_mode)),
+          IconButton(
+              onPressed: () => controller.changeTheme(ThemeMode.system),
+              icon: const Icon(Icons.settings)),
         ],
       ),
     );

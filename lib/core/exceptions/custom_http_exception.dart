be2bc86f35dd0d0ebfc64efb5585299d8bf1d0d4
bthnkucuk <84974284+bthnkucuk@users.noreply.github.com>
2023-04-27 18:05:04 +0300

    custom http message log

--- lib/core/exceptions/custom_http_exception.dart
+++ lib/core/exceptions/custom_http_exception.dart
@@ -1,6 +1,12 @@
+import 'package:base_application/core/logger.dart';
+
 class CustomHttpException implements Exception {
   final String? message;
   final int? statusCode;
 
-  CustomHttpException({this.statusCode, this.message});
+  CustomHttpException({this.statusCode, this.message}) {
+    Log.error(
+      'Custom HTTP Exeption! Status Code: $statusCode Error Message: $message',
+    );
+  }
 }
